0dff1d2f7c20d687a7316deb3fdf06892407c319
damianlarson <damian.l.larson@gmail.com>
2021-08-14 15:57:19 -0500

    add db, pull all players+by adp+by service

--- route/playerRoutes.js
+++ route/playerRoutes.js
@@ -1,9 +1,26 @@
-const getPlayers = (req, res) => {
-  res.send('Getting players');
+const db = require('../db/index');
+
+const getAllPlayers = async (req, res) => {
+  const { rows } = await db.query('SELECT id, full_name, player_position FROM players');
+  res.send(rows);
 };
 
-const getPlayersRoute = (router) => {
-  router.get('/adp', getPlayers);
+const getADPs = async (req, res) => {
+  const { scoringFormat } = req.params;
+  const { service } = req.params || null;
+  try {
+    const { rows } = await db.query('SELECT full_name, player_position, team, adp, adp_formatted, high, low, stdev, bye, service FROM players INNER JOIN adp ON (players.id = player_id) WHERE scoring = $1 and ($2::text IS NULL or service = $2::text) ORDER BY adp ASC', [scoringFormat.toUpperCase(), service]);
+    res.send(rows);
+  } catch (err) {
+    res.status(400);
+    console.error(err);
+    res.send({ error: 'Invalid format' });
+  }
 };
 
-module.exports = getPlayersRoute;
+const playerRoutes = (router) => {
+  router.get('/', getAllPlayers);
+  router.get('/adp/:scoringFormat/:service?', getADPs);
+};
+
+module.exports = playerRoutes;
--- route/routes.js
+++ route/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const playerRouter = express.Router({ mergeParams: true });
 
-require('./playerRoutes')(router);
+require('./playerRoutes')(playerRouter);
 
+router.use('/players', playerRouter);
 module.exports = router;
