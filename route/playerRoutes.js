4cb7c3da275a21bf00979daaf846e3a56d727759
damianlarson <damian.l.larson@gmail.com>
2021-08-18 12:48:53 -0500

    fix query to use vaccine player attrs

--- route/playerRoutes.js
+++ route/playerRoutes.js
@@ -9,7 +9,7 @@ const getADPs = async (req, res) => {
   const { scoringFormat } = req.params;
   const { service } = req.params || null;
   try {
-    const { rows } = await db.query('SELECT full_name, player_position, team, adp, adp_formatted, high, low, stdev, bye, service FROM players INNER JOIN adp ON (players.id = player_id) WHERE scoring = $1 and ($2::text IS NULL or service = $2::text) ORDER BY adp ASC', [scoringFormat.toUpperCase(), service]);
+    const { rows } = await db.query('SELECT full_name, player_position, team, adp, adp_formatted, high, low, stdev, bye, vaccination_status, vaccination_reference, is_vaccinated FROM players INNER JOIN adp ON (players.id = adp.player_id) WHERE scoring = $1 and ($2::text IS NULL or service = $2::text) ORDER BY adp ASC', [scoringFormat.toUpperCase(), service]);
     res.send(rows);
   } catch (err) {
     res.status(400);
